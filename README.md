62e89b5ee00b3f9619df2812dec7cf59167559db
Stephen Nielsen <smnielsen256@byui.edu>
2023-11-16 16:50:10 -0700

    Structured generate_report()

--- README.md
+++ README.md
@@ -13,26 +13,12 @@
 ## Files
 * main.py
 * db_interface.py - holds all the functions for interfacing with the database
-    * create_new_database() 
-    * start_database(username, user_password, host_address, db_name)
-    * close_database(cnx)
-    * create_new_report()
-    * edit_reports(report)
-    * view_reports(report)
-    * create_new_patient()
-    * edit_patients(patient)
-    * create_new_nutrient()
-    * edit_nutrients(nutrient)
-    * create_new_supplement()
-    * edit_supplements(supplement)
-    * create_new_medication()
-    * edit_medications(medication)
-    * create_new_ref_chart()
-    * edit_ref_charts(ref_chart)
-
 * README.md
 
 ## Reference
 * MySQL Connector, the Python/MySQL interface: https://dev.mysql.com/doc/connector-python/en/
        * Guidelines: https://dev.mysql.com/doc/connector-python/en/connector-python-coding.html 
        * W3 resource: https://www.w3schools.com/python/python_mysql_getstarted.asp 
+* Medical calculations
+       * Holliday-Segar: https://www.msdmanuals.com/professional/multimedia/table/holliday-segar-formula-for-maintenance-fluid-requirements-by-weight 
+       * WHO REE: http://www.nafwa.org/who_equation.html 
--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         # user input
         print("--------------------")
         print("What would you like to do?")
-        user_input = input("1) Exit 2) Create 3) Read 4) Update 5) Delete: ")
+        user_input = input("1) Exit 2) Create 3) Read 4) Update 5) Delete 6) Generate report: ")
         print()
         try:
             user_input = int(user_input)
@@ -52,7 +52,7 @@ def main():
                 create(cnx, test_tablename, test_dict)
             case 3:
                 # if read
-                returned = read(cnx, get_table_names_interface(cnx, config))
+                returned = read(cnx, get_table_names_interface(cnx, config), where=f"{test_tablename}_id = 5")
                 if returned.empty:
                     print("Table is empty")
                 else:
@@ -63,7 +63,8 @@ def main():
             case 5:
                 # if delete
                 delete(cnx, test_tablename, get_table_items_interface(cnx, test_tablename))
-                #print(get_table_items_interface(cnx, test_tablename))
+            case 6:
+                generate_report()
             case _:
                 # else
                 print("Invalid input.")
@@ -77,7 +78,81 @@ def main():
     # end program
     print("TERMINATED")
     
+def generate_report():
+    """
+    creates a report
+    * Parameters:none
+    * Returns: 
+           * report: json
+    """
+    report = {
+        "header": {
+            "name": "",
+            "sex": "",
+            "DOB": "",
+            "age": 0.0,
+            "weight_kg": 0.0,
+            "current_date": "",
+            "feeding_schedule": "",
+            "method_of_delivery": "",
+            "home_recipe": "",
+            "fluids": "",
+            "solids": ""
+        },
+        "food_and_supplements": {
+            "": 0.0,
+            "total_formula_only": 0.0,
+            "total_food_and_formula": 0.0,
+        },
+        "calculations": {
+            "Holliday-Segar": {
+                "maintenance": 0.0,
+                "sick_day": 0.0
+            },
+            "WHO_REE": 0.0
+        }
+    }
 
+    # TODO import patient data
+
+    # calculate Holliday-Segar formula
+    hs = 0.0
+    weight = report["header"]["weight_kg"]
+
+    if weight <= 10.0:
+        hs = weight * 100.0
+    elif weight <= 20.0:
+        hs = 1000 + (50 * (weight - 10))
+    else:
+        hs = 1500 + (20 * (weight - 20))
+
+    report["calculations"]["Holliday-Segar"]["maintenance"] = hs
+    report["calculations"]["Holliday-Segar"]["sick_day"] = hs * 1.5
+
+    # calculate WHO REE formula
+    sex = report["header"]["sex"]
+    age = report["header"]["age"]
+    wr = 0.0
+
+    if sex == "M":
+        if age <= 3:
+            wr = (weight * 60.9) - 54
+        elif age <= 10:
+            wr = (weight * 22.7) + 495
+        else:
+            wr = (weight * 17.5) + 651
+    elif sex == "F":
+        if age <= 3:
+            wr = (weight * 60.1) - 51
+        elif age <= 10:
+            wr = (weight * 22.5) + 499
+        else:
+            wr = (weight * 12.2) + 746
+    else:
+        print(f"Unknown sex '{sex}'")
+    
+    report["calculations"]["WHO_REE"] = wr
+    
 
 
 if __name__ == '__main__':
